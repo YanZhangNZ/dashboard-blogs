3f9e9f88b7ea80413a3ea162bbda3fa948afbc45
YanZhangNZ <crazyanz888@gmail.com>
2019-05-21 17:30:44 +1200

    sign in component

---
+++ src/components/auth/SignIn.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+
+export default class SignIn extends Component {
+    state = {
+
+    }
+    handleSubmit = () => {
+        console.log('submit:')
+    }
+    handleChange = () => {
+        console.log('input:')
+    }
+    render(){
+        return(
+            <div className="container">
+                <form onSubmit={this.handleSubmit} className="white">
+                    <h5 className="grey-text text-darken-3">sign in</h5>
+                    <div className="input-field">
+                        <label htmlform='email'>Email</label>
+                        <input type="email" id="email" onChange={this.handleChange} />
+                    </div>
+                    <div className="input-field">
+                        <label htmlform='password'>password</label>
+                        <input type="password" id="password" onChange={this.handleChange} />
+                    </div>
+                    <div className="input-field">
+                        <button className="btn pink lighten-1 z-depth-0">Login</button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+
+}
--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -5,11 +5,11 @@ const ProjectDetails = (props) => {
     console.log('get id for project detail from props:',id);
 
   return (
-      <div class="container section project-details">
-          <div class="card z-depth-0">
-            <div class="card-content">
-              <h4 class="card-title">project title{id}</h4>
-              <p class="card-text">......</p>
+      <div className="container section project-details">
+          <div className="card z-depth-0">
+            <div className="card-content">
+              <h4 className="card-title">project title - {id}</h4>
+              <p className="card-text">......</p>
             </div>
             <div className="card-action grey lighten-4 grey-text">
                 <div>Posted by: *** </div>
