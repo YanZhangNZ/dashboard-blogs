e123c60acf575b9f9c8d500c942bd92f3d802ca3
YanZhangNZ <crazyanz888@gmail.com>
2019-05-28 23:46:06 +1200

    fix logOut with signOut function

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -2,25 +2,25 @@
 
 import React from 'react';
 import {NavLink} from 'react-router-dom';
-
+import { connect } from 'react-redux';
+import { signOut } from '../../store/actions/authActions';
 
 const SignedInLinks = (props) => {
     return(
         <ul className="right">
             <li><NavLink to='/create' >New Project</NavLink></li>
-            <li><a href="/">Log Out</a></li>
+            <li><span onClick={props.signOut} >Log Out</span></li>
             <li><NavLink to='/' className="btn btn-floating pink lighten-1">YZ</NavLink></li>
         </ul>
 
     )
 }
 
-// const mapDispatchToProps = (dispatch) => {
-//     return{
-//         signOut: () => dispatch(signOut())
-//     }
-// }
+const mapDispatchToProps = (dispatch) => {
+    return{
+        signOut: () => dispatch(signOut())
+    }
+}
 
-// export default connect(null,mapDispatchToProps)(SignedInLinks);
+export default connect(null,mapDispatchToProps)(SignedInLinks);
 
-export default SignedInLinks;
