e58d7f3c5f4651cf7280190cf82cc483814ac4c1
YanZhangNZ <crazyanz888@gmail.com>
2019-05-31 17:14:33 +1200

    replace logo

---
+++ dist/precache-manifest.372bdfa7d2be3e26113a73ca3fa7a353.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fe70d1b9bd184f92d76e",
+    "url": "/static/js/main.fe70d1b9.chunk.js"
+  },
+  {
+    "revision": "77394b4b5281657313b0",
+    "url": "/static/js/1.77394b4b.chunk.js"
+  },
+  {
+    "revision": "fe70d1b9bd184f92d76e",
+    "url": "/static/css/main.ff6e723d.chunk.css"
+  },
+  {
+    "revision": "958fe9da72163f0a6ccc31e20ecf07e6",
+    "url": "/index.html"
+  }
+];
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,16 +5,18 @@ import {Link} from 'react-router-dom';
 import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
 import { connect } from 'react-redux'
+import logo from '../../logo.png';
 
 // these two signin and signout components will show under some logic authentication
 const Navbar = (props) => {
     // console.log('props in navbar:',props);
     const { auth, profile } = props;
     const links = auth.uid ? <SignedInLinks profile={profile}/>:<SignedOutLinks />
+
     return(
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
-                <Link to="/" className="brand-logo">Dashboard</Link>
+                <Link to="/" className="brand-logo" style={{left:'55px'}}><img src={logo} alt='logo' style={{width:'50px',height:'50px',background:'white',borderRadius:'50%',margin:'5px auto'}}/></Link>
                 { links }
             </div>
         </nav>
