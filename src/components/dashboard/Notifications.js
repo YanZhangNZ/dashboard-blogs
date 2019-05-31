7f0af356460b2b354d31002b567f886d3fed59f9
YanZhangNZ <crazyanz888@gmail.com>
2019-05-31 16:35:13 +1200

    make cloud functions to doc notification in dabastore

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const Notifications = () => {
+const Notifications = (props) => {
     return(
-        <div>
-            <p>Notifications</p>
+        <div className="section">
+            <div className="card z-depth-0">
+                <div className="card-content">
+                    <span className="card-title">Notifications</span>
+                    <ul>
+                        <li>noti</li>
+                        <li>noti</li>
+                        <li>noti</li>
+                        <li>noti</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
